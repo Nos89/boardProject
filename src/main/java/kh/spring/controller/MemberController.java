028582d335387beda193207b1c926d2cedc6e7f7
nos <alukard666@naver.com>
2021-01-08 11:25:35 +0900

    Login 기능 구현

--- src/main/java/kh/spring/controller/MemberController.java
+++ src/main/java/kh/spring/controller/MemberController.java
@@ -1,8 +1,37 @@
 package kh.spring.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kh.spring.service.MemberService;
 
 @Controller
+@RequestMapping("/member")
 public class MemberController {
 
+	@Autowired
+	HttpSession session;
+	
+	@Autowired
+	MemberService mservice;
+	
+	
+	@RequestMapping("login")
+	public String login(String id, String pw, Model model) {
+		System.out.println(id + pw);
+		boolean result = mservice.login(id, pw);
+		System.out.println(result);
+		if( result ) {
+			session.setAttribute("loginID", id);
+			model.addAttribute("loginMsg", "success");
+		} else {
+			model.addAttribute("loginMsg", "fail");
+		}
+		return "home";
+	}
+	
 }
--- src/main/java/kh/spring/service/MemberService.java
+++ src/main/java/kh/spring/service/MemberService.java
@@ -1,8 +1,22 @@
 package kh.spring.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kh.spring.dao.MemberDAO;
 
 @Service
 public class MemberService {
-
+	@Autowired
+	MemberDAO mdao;
+	
+	public boolean login( String id, String pw ) {
+		Map<String, String> temp = new HashMap<>();
+		temp.put("id", id);
+		temp.put("pw", pw);
+		return mdao.login(temp);
+	}
 }
