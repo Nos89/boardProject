266f1da4e4ce6d791d1840bb387fc14aa6c8b7c9
HJ <hyeonj0130@gmail.com>
2021-01-08 15:00:09 +0900

    Sign Up Created

--- src/main/java/kh/spring/dao/MemberDAO.java
+++ src/main/java/kh/spring/dao/MemberDAO.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import kh.spring.dto.MemberDTO;
+
 @Component
 public class MemberDAO {
 	@Autowired
@@ -19,5 +21,15 @@ public class MemberDAO {
 		} else {
 			return false;
 		}
+	}
+	
+	// 아이디 중복 체크
+	public int isIdDuplicated(String id) {
+		return db.selectOne("Member.isIdDuplicated", id);
+	}
+	
+	// 회원가입
+	public int signUp(MemberDTO dto) {
+		return db.insert("Member.signUp",dto);
 	}
 }
--- src/main/java/kh/spring/service/MemberService.java
+++ src/main/java/kh/spring/service/MemberService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kh.spring.dao.MemberDAO;
+import kh.spring.dto.MemberDTO;
 
 @Service
 public class MemberService {
@@ -18,5 +19,15 @@ public class MemberService {
 		temp.put("id", id);
 		temp.put("pw", pw);
 		return mdao.login(temp);
+	}
+	
+	// 아이디 중복확인
+	public int isIdDuplicated(String id) {
+		return mdao.isIdDuplicated(id);
+	}
+	
+	// 회원가입
+	public int signUp(MemberDTO dto) {
+		return mdao.signUp(dto);
 	}
 }
