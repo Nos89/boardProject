98e494a09fc0dc4b01d365a42f44ab407736e1b3
nos <aluakrd666@naver.com>
2021-01-08 15:14:44 +0900

    수정버전

--- src/main/java/kh/spring/controller/BoardController.java
+++ src/main/java/kh/spring/controller/BoardController.java
@@ -16,19 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import kh.spring.dto.CommentDTO;
 import kh.spring.service.BoardService;
 
-
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
 	@Autowired
 	private HttpSession session;
-	
+
 	@Autowired
 	private BoardService bservice;
-	
+
 	private int board_seq;
-	
+
 	@RequestMapping("showBoard.brd")
 	public String showBoard(int cpage) throws Exception {
 		int amount = bservice.listBoard().size();
@@ -39,7 +38,7 @@ public class BoardController {
 		session.setAttribute("navi", navi);
 		return "board/board";
 	}
-	
+
 	// 글쓰기 창으로 이동
 	@RequestMapping("toWrite")
 	public String toBoardWrite() {
@@ -60,41 +59,41 @@ public class BoardController {
 		return null;
 	}
 
-	//글 상세보기(+댓글 확인)
+	// 글 상세보기(+댓글 확인)
 	@RequestMapping("/boardDetail")
-	public String boardDetail(Model model,HttpServletRequest req){
+	public String boardDetail(Model model, HttpServletRequest req) {
 		int seq = Integer.parseInt(req.getParameter("seq"));
 		board_seq = seq;
-		String loginId = (String)req.getSession().getAttribute("loginID");
-		
+		String loginId = (String) req.getSession().getAttribute("loginID");
+
 		BoardDTO dto = bservice.viewBoardDetail(seq);
 		List<CommentDTO> list = bservice.cmtList(board_seq);
-		
+
 		model.addAttribute("dto", dto);
 		model.addAttribute("loginId", loginId);
 		model.addAttribute("cmtList", list);
 		return "/board/boardDetail";
 	}
-	
-	//댓글 달기
-		@RequestMapping("writeCmt")
-		public String writeCmt( Model model,String contents) {
-			String writer = (String) session.getAttribute("loginID");
 
-			try {
-				int result = bservice.writeCmt(board_seq,writer, contents);
-				System.out.println(result);
-				BoardDTO dto = bservice.viewBoardDetail(board_seq);
-				List<CommentDTO> list = bservice.cmtList(board_seq);
-				
-				model.addAttribute("dto", dto);
-				model.addAttribute("loginId", writer);
-				// model.addAttribute("result", result);
-				return "/board/boardDetail";
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return null;
+	// 댓글 달기
+	@RequestMapping("writeCmt")
+	public String writeCmt(Model model, String contents) {
+		String writer = (String) session.getAttribute("loginID");
+
+		try {
+			int result = bservice.writeCmt(board_seq, writer, contents);
+			System.out.println(result);
+			BoardDTO dto = bservice.viewBoardDetail(board_seq);
+			List<CommentDTO> list = bservice.cmtList(board_seq);
+
+			model.addAttribute("dto", dto);
+			model.addAttribute("loginId", writer);
+			// model.addAttribute("result", result);
+			return "/board/boardDetail";
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		return null;
+	}
 
 }
